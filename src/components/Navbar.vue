045ce7b1786e9be4975676030d61e7743b3a2c7b
thierryrudaseswa <trudaseswa@gmail.com>
2024-09-05 23:06:31 +0200

    The set up of the vue jobs blog app in vue js

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<script setup>
+import logo from '@/assets/img/logo.png';
+</script>
+
+<template>
+  <nav class="bg-green-700 border-b border-green-500">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="flex h-20 items-center justify-between">
+        <div
+          class="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
+        >
+          <!-- Logo -->
+          <a class="flex flex-shrink-0 items-center mr-4" href="/">
+            <img class="h-10 w-auto" :src="logo" alt="Vue Jobs" />
+            <span class="hidden md:block text-white text-2xl font-bold ml-2">
+              Vue Jobs
+            </span>
+          </a>
+
+          <div class="md:ml-auto">
+            <div class="flex space-x-2">
+              <a
+                href="/"
+                class="text-white px-3 py-2 rounded-md hover:bg-gray-900 hover:text-white"
+              >
+                Home
+              </a>
+              <a
+                href="/jobs"
+                class="text-white px-3 py-2 rounded-md hover:bg-gray-900 hover:text-white"
+              >
+                Jobs
+              </a>
+              <a
+                href="/jobs/add"
+                class="text-white px-3 py-2 rounded-md hover:bg-gray-900 hover:text-white"
+              >
+                Add Job
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './assets/main.css'
+import { createRouter, createWebHistory } from 'vue-router';
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
