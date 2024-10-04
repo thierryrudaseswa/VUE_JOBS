f24367fd71e9617227a171c17ca57776ae2e80cb
thierryrudaseswa <trudaseswa@gmail.com>
2024-10-04 22:08:49 +0200

    The HomeCard being updated

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -40,7 +40,7 @@ const truncatedDescription = computed(()=>{
 
       <h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
 
-      <div class="border border-gray-100 mb-5 p-0"></div>
+      <div class="border border-gray-100 mb-5"></div>
 
       <div class="flex flex-col lg:flex-row justify-between mb-4">
         <div class="text-orange-700 mb-3">
--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -20,7 +20,7 @@ defineProps({
 
 <template>
   <section class="bg-blue-50 px-4 py-10">
-    <div class="container-xl lg:container m-auto p-">
+    <div class="container-xl lg:container m-auto">
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
       </h2>
